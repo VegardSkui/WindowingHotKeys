c773f3be2348bcde19a7e2eefae227082e0312fc
Vegard Skui <me@vegardskui.com>
2020-06-01 13:45:51 +0200

    Initial commit

---
+++ WindowingHotKeys/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  WindowingHotKeys
+//
+//  Created by Vegard Skui on 01/06/2020.
+//  Copyright © 2020 Vegard Skui. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+}
+
