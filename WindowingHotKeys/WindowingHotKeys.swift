b54c306b57bae75f72dac928dc27e8a581db3ef4
Vegard Skui <me@vegardskui.com>
2020-12-02 11:32:03 +0100

    Limit the application to a single running instance

--- WindowingHotKeys/WindowingHotKeys.swift
+++ WindowingHotKeys/WindowingHotKeys.swift
@@ -11,8 +11,34 @@ import Cocoa
 @main
 struct WindowingHotKeys {
     static func main() {
+        // Limit the application to a single running instance, multiple
+        // instances may occur if there are multiple copies of the app bundle
+        terminateOtherRunningInstances()
+
         let delegate = AppDelegate()
         NSApplication.shared.delegate = delegate
         NSApplication.shared.run()
+    }
+
+    /// Terminates every other running instance of the application for the current user.
+    private static func terminateOtherRunningInstances() {
+        guard let bundleIdentifier = Bundle.main.bundleIdentifier else { return }
+        let runningInstances = NSRunningApplication.runningApplications(withBundleIdentifier: bundleIdentifier)
+
+        // If there is only one running instance, it must be this one
+        guard runningInstances.count != 1 else { return }
+
+        let currentProcessIdentifier = NSRunningApplication.current.processIdentifier
+        runningInstances.forEach { instance in
+            // Skip the current instance
+            guard instance.processIdentifier != currentProcessIdentifier else { return }
+
+            let success = instance.terminate()
+            if success {
+                print("Terminated running instance with pid \(instance.processIdentifier)")
+            } else {
+                print("Could not terminate running instance with pid \(instance.processIdentifier)")
+            }
+        }
     }
 }
